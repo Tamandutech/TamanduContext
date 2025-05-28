65bf07fcf0e26d86d58049e5254a8476111ab913
Alexandre Hiroyuki <alex.hiroyuki@outlook.com>
2025-05-27 23:54:05 -0300

    feat: add tamandu context lib (#2)
    * feat: add tamandu context lib
    
    * chore: remove unused comments
    
    * chore: fix linter
    
    * ci: fix linter
    
    * ci: update
    
    * style: clean unused code
    
    * fix: remove ununsed code
    
    * fix: initialize props

--- build_test.cpp
+++ build_test.cpp
@@ -1,9 +1,17 @@
 // Include lib:
+#include "TamanduContext.hpp"
 #include <Arduino.h>
+
+Tamandu::Context<int> *context;
 
 void setup() {
   // Initialize serial interface
   Serial.begin(9600);
+
+  context = new Tamandu::Context<int>("test");
 }
 
-void loop() { delay(1000); }
+void loop() {
+  context->set(10);
+  delay(1000);
+}
---
+++ src/TamanduContext.hpp
@@ -0,0 +1,87 @@
+#ifndef DATA_ABSTRACT_HPP
+#define DATA_ABSTRACT_HPP
+
+#include <atomic>
+#include <cstdlib>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <vector>
+
+namespace Tamandu {
+
+template <class T> class Context {
+public:
+  Context(std::string name);
+  Context(std::string name, T value);
+  virtual ~Context();
+
+  T get();
+  void set(T data);
+  void set(std::string data);
+
+  std::string getName();
+  std::string getString(std::string ctrl);
+
+protected:
+  std::string name;
+
+  std::string demangle(const char *mangled) {
+    int status;
+    std::unique_ptr<char[], void (*)(void *)> result(
+        abi::__cxa_demangle(mangled, 0, 0, &status), std::free);
+    return result.get() ? std::string(result.get()) : "error occurred";
+  };
+
+private:
+  std::atomic<T> *data;
+};
+
+template <class T> Context<T>::Context(std::string name) : name(name) {
+  this->data = new std::atomic<T>();
+}
+
+template <class T> Context<T>::Context(std::string name, T value) : name(name) {
+  this->data = new std::atomic<T>(value);
+}
+
+template <class T> Context<T>::~Context() { delete this->data; }
+
+template <class T> std::string Context<T>::getName() { return this->name; }
+
+template <class T> T Context<T>::get() {
+  return this->data->load(std::memory_order_acquire);
+}
+
+template <class T> std::string Context<T>::getString(std::string ctrl) {
+  // Create an output string stream
+  // std::ostringstream streamObj;
+  // Add data to stream
+  // streamObj << this->data->load(std::memory_order_acquire);
+  // Get string from output string stream
+  // std::string strObj = streamObj.str();
+  // streamObj.str("");
+
+  char buffer[64];
+  memset(buffer, 0, sizeof(buffer));
+  std::string strObj(buffer);
+  return strObj;
+}
+
+template <class T> void Context<T>::set(T data) {
+  this->data->store(data, std::memory_order_release);
+  uint32_t last_change = xTaskGetTickCount() * portTICK_PERIOD_MS;
+}
+
+template <class T> void Context<T>::set(std::string data) {
+  double num = std::stod(data);
+  this->data->store(num, std::memory_order_release);
+  uint32_t last_change = xTaskGetTickCount() * portTICK_PERIOD_MS;
+
+  char buffer[64];
+  memset(buffer, 0, sizeof(buffer));
+  std::string strObj(buffer);
+}
+} // namespace Tamandu
+
+#endif
